3cdc05633536ee433c0b14962fa505327d74a886
kitakenkani <kentaro.kitamura.2p@stu.hosei.ac.jp>
2024-02-20 12:56:57 +0900

    table

--- src/app/page.js
+++ src/app/page.js
@@ -25,7 +25,78 @@ export default function Home() {
                         <AudioSample title="BinauralS4(Our)" src="https://storage.googleapis.com/mos_example/mos/binaurals4/subject1.wav" />
                     </div>
                 </div>
-
+                <div className="overflow-x-auto">
+                    <table className="table-auto w-full mb-8">
+                        <caption className="text-lg font-semibold">モデルによる性能の違い</caption>
+                        <thead>
+                            <tr className="bg-gray-100">
+                                <th>Models</th>
+                                <th>L2(&times;10<sup>-3</sup>)</th>
+                                <th>Amp</th>
+                                <th>Phase</th>
+                                <th>MRSTFT</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>DSP</td>
+                                <td>0.725</td>
+                                <td>0.060</td>
+                                <td>1.584</td>
+                                <td>2.140</td>
+                            </tr>
+                            <tr className="bg-gray-50">
+                                <td>Warpnet</td>
+                                <td>0.144</td>
+                                <td>0.036</td>
+                                <td><strong>0.804</strong></td>
+                                <td>1.755</td>
+                            </tr>
+                            <tr>
+                                <td>BinauralGrad</td>
+                                <td>0.129</td>
+                                <td><strong>0.030</strong></td>
+                                <td>0.837</td>
+                                <td><strong>1.282</strong></td>
+                            </tr>
+                            <tr className="bg-gray-50">
+                                <td><strong>Ours</strong></td>
+                                <td><strong>0.121</strong></td>
+                                <td>0.032</td>
+                                <td>0.851</td>
+                                <td>1.747</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <table className="table-auto w-full">
+                        <caption className="text-lg font-semibold">トレーニングデータを半分にして学習したモデルによる比較</caption>
+                        <thead>
+                            <tr className="bg-gray-100">
+                                <th>Models</th>
+                                <th>L2(&times;10<sup>-3</sup>)</th>
+                                <th>Amp</th>
+                                <th>Phase</th>
+                                <th>MRSTFT</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>BinauralGrad</td>
+                                <td>0.142</td>
+                                <td>0.037</td>
+                                <td>1.080</td>
+                                <td><strong>1.251</strong></td>
+                            </tr>
+                            <tr className="bg-gray-50">
+                                <td><strong>Ours</strong></td>
+                                <td><strong>0.126</strong></td>
+                                <td><strong>0.032</strong></td>
+                                <td><strong>0.881</strong></td>
+                                <td>1.750</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </>
     );
